6e73a112e118b2a59e33bf9dd27daadd9e1296bf
huskylin <hank84112092@gmail.com>
2018-05-11 18:08:00 +0800

    初始版本

---
+++ FBlottery.py
@@ -0,0 +1,38 @@
+#-*- coding:big5 -*- 
+
+#   �׺ݾ��ýX   
+#   chcp 65001
+
+import requests
+import json
+import random
+
+#  Facebook API
+url = 'https://graph.facebook.com/v2.12/'
+FB_token = '�ܼ�'
+fileds = '2104358166245473/comments?filter=stream&limit=1000&order=chronological'
+
+response = requests.get(url + fileds + FB_token).json()
+
+#   get username form response data
+result = list()
+for comments in response['data']:
+    result.append({
+        'name' : comments['from']['name']
+    })
+
+#   remove duplicates
+seen = set()
+new_l = []
+for d in result:
+    t = tuple(d.items())
+    if t not in seen:
+        seen.add(t)
+        new_l.append(d)
+
+#   random sort
+random.shuffle(new_l)
+bingo = new_l[:1]
+
+#   print
+print(bingo)
